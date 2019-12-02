8d06afdadd3c9115606bb2b64fa01d037442e562
Amir Dekel <6893125+adql@users.noreply.github.com>
2019-12-02 15:09:56 +0100

    Cosmetic changes and a comment

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,16 +1,16 @@
 (ns alphabet-cipher.coder
   (:require [clojure.string :as s]))
 
-(defn- str->bytecode
-  "Translates a string into a bytecode sequence where 'a'=0"
+(defn- str->num
+  "Translates a string into a numeric representation of the letters"
   [s]
   (map #(- % 97) (.getBytes (s/lower-case s))))
 
 (defn- execute
   "One function does it all by taking :encode or :decode as operation"
   [keyword message operation]
-  (let [keys         (cycle (str->bytecode keyword))
-        byte-message (str->bytecode message)
+  (let [keys         (cycle (str->num keyword))
+        byte-message (str->num message)
         op           (operation {:encode #(char (+ 97 (mod (+ %1 %2) 26)))
                                  :decode #(char (+ 97 (mod (+ 26 (- %1 %2)) 26)))})]
     (apply str (map op byte-message keys))))
@@ -21,6 +21,9 @@
 (defn decode [keyword message]
   (execute keyword message :decode))
   
+;; The nice trick here is that using the message as a keyword to
+;; decode the cipher provides the original keyword in repetition, from
+;; which the single keyword is extracted
 (defn decipher [cipher message]
   (let [repeated-keyword (decode message cipher)]
     (loop [cand-len 1]
